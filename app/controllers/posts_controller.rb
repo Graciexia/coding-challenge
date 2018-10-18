29d1b80cffa2d32bdd9bcc8a03cb11a58d5bcaa0
xiawei <graciexia@outlook.com>
2018-10-18 00:34:50 -0500

    fished post controller and most of views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
-  def index
-    # Return all `Post`
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
+  def index
+    if params[:search].present?
+      @posts = Post.where("title LIKE ? OR body LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").all.order('created_at DESC')
+      @search_performed = true
+    else
+      @posts = Post.all.order("created_at DESC")
+      @search_performed = false
+    end
   end
 
   def new
@@ -9,10 +16,42 @@ class PostsController < ApplicationController
   end
 
   def create
-    # Add a new `Post` to the database
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    # Remove a `Post` from the database
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.friendly.find(params[:id])
   end
 end
